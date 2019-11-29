6a883344ae0c25ba25afa6aa84ecb0e55d12bd57
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-11-28 23:24:21 -0500

    shirt adjustments

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 let minusBut = document.getElementById("pantsMinusBut");
 let addBut = document.getElementById("pantsAddBut");
+let minus_But = document.getElementById("shirtsMinusBut");
+let add_But = document.getElementById("shirtsAddBut");
 
 function minus () {
     let label = document.getElementById ("pantsQty");
+    label.innerHTML = parseInt (label.innerHTML) -1;
+}
+
+function minus_shirt () {
+    let label = document.getElementById ("shirtsQty");
     label.innerHTML = parseInt (label.innerHTML) -1;
 }
 
@@ -11,8 +18,15 @@ function add () {
     label.innerHTML = parseInt (label.innerHTML) +1;
 }
 
+function add_shirt () {
+    let label = document.getElementById ("shirtsQty");
+    label.innerHTML = parseInt (label.innerHTML) +1;
+}
+
 minusBut.addEventListener ("click", minus);
 addBut.addEventListener ("click", add);
+minus_But.addEventListener ("click", minus_shirt);
+add_But.addEventListener ("click", add_shirt);
 
 let calculPrice = document.getElementById ("calculatorBut");
 
