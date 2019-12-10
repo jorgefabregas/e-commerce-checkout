59bd154125be9d25e3b6ff089dea393f40ddadc7
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-12-09 23:19:34 -0500

    totals

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ let shirtsQty = 0;
 let shirtsPri = 10.99;
 
 document.getElementById ('pantsQty').innerHTML = `${pantsQty}`;
-document.getElementById ('pants_price').innerHTML = `$${pantsPri}`;
+document.getElementById ('pants_price').innerHTML ="$" +  `${pantsPri}`;
 document.getElementById ('pants_extendpri').innerHTML = `${extPrice}`;
 
 document.getElementById ('shirtsQty').innerHTML = `${shirtsQty}`;
@@ -22,7 +22,7 @@ document.getElementById ('shirt_price').innerHTML = `$${shirtsPri}`;
 document.getElementById ('shirt_extendpri').innerHTML = `${extPrice}`;
 
 function minus (itemsQty) {
-    if (itemsQty >= 0) {
+    if (itemsQty <= 0) {
         return 0;}
     else {
         return (--itemsQty);}
@@ -30,17 +30,17 @@ function minus (itemsQty) {
 function add (itemsQty) {
     return (++itemsQty);
 }
-function giveExtPrice (price, itemsQty) {
-
+function giveExtPrice(price, itemsQty){
+    return(price * itemsQty);
 }
 
 function minusPants () {
     let minusPants = minus(document.getElementById ('pantsQty').innerHTML);
     let pantsPri = 40.99;
-    let extPrice = giveExtPrice(minusPants,pantsPri);
+    let extPrice = giveExtPrice(pantsPri, minusPants);
 
     document.getElementById ('pantsQty').innerHTML = `${minusPants}`;
-    document.getElementById ('pants_price').innerHTML = `${pantsPri}`;
+    document.getElementById ('pants_price').innerHTML = "$" + `${pantsPri}`;
     document.getElementById ('pants_extendpri').innerHTML = `${extPrice.toFixed (2)}`;
 }
 function addPants () {
@@ -49,7 +49,7 @@ function addPants () {
     let extPrice = giveExtPrice(addPants,pantsPri);
 
     document.getElementById ('pantsQty').innerHTML = `${addPants}`;
-    document.getElementById ('pants_price').innerHTML = `${pantsPri}`;
+    document.getElementById ('pants_price').innerHTML = "$" +  `${pantsPri}`;
     document.getElementById ('pants_extendpri').innerHTML = `${extPrice.toFixed (2)}`;
 }
 function minusShirts () {
@@ -58,7 +58,7 @@ function minusShirts () {
     let extPrice = giveExtPrice(minusShirts,shirtsPri);
 
     document.getElementById ('shirtsQty').innerHTML = `${minusShirts}`;
-    document.getElementById ('shirt_price').innerHTML = `${shirtsPri}`;
+    document.getElementById ('shirt_price').innerHTML = "$" +  `${shirtsPri}`;
     document.getElementById ('shirt_extendpri').innerHTML = `${extPrice.toFixed (2)}`;
 }
 function addShirts () {
@@ -67,7 +67,7 @@ function addShirts () {
     let extPrice = giveExtPrice(addShirts,shirtsPri);
 
     document.getElementById ('shirtsQty').innerHTML = `${addShirts}`;
-    document.getElementById ('shirt_price').innerHTML = `${shirtsPri}`;
+    document.getElementById ('shirt_price').innerHTML = "$" +  `${shirtsPri}`;
     document.getElementById ('shirt_extendpri').innerHTML = `${extPrice.toFixed (2)}`;
 }
 minusBut.addEventListener ("click", minusPants);
@@ -76,32 +76,37 @@ minus_But.addEventListener ("click", minusShirts);
 add_But.addEventListener ("click", addShirts);
 
 function calculTotals () {
-    let pantsExtPrice = parseFloat (document.getElementById ('pants_extendpri').innerHTML);
-    let shirtsExtPrice = parseFloat (document.getElementById ('shirt_extendpri').innerHTML);
+    let pantsExtPrice = parseFloat(document.getElementById ('pants_extendpri').innerHTML);
+    let shirtsExtPrice = parseFloat(document.getElementById ('shirt_extendpri').innerHTML);
     let subtotal = pantsExtPrice + shirtsExtPrice;
     let tax = subtotal * 13/100;
     let total = subtotal + tax;
-    let coupon = document.getElementById ('promoText').value;
-    let validation = validCode (coupon);
 
-    if (validation == "NOTAX") {
-        tax = 0;
-        total = subtotal;}
-    
-    if (validation == "FIFTYFIFTY") {
-        subtotal = (pantsExtPrice + shirtsExtPrice)/2;
-        tax = subtotal * 13/100;
-        total = subtotal + tax;}
 
-    if (validation == false) {
-        document.getElementById ('promoButton').innerHTML = "Please, Enter a Valid Coupon";}
+    document.getElementById ('subtotal').innerHTML ="Subtotal: $" + `${subtotal.toFixed(2)}`;
+    document.getElementById ('tax').innerHTML = "Tax: $" + `${tax.toFixed(2)}`;
+    document.getElementById ('total').innerHTML = "Total: $" + `${total.toFixed(2)}`;        
 }
 
-document.getElementById ('subtotal').innerHTML = `${subtotal.toFixed(2)}`;
-document.getElementById ('tax').innerHTML = `${tax.toFixed(2)}`;
-document.getElementById ('total').innerHTML = `${total.toFixed(2)}`;
+calcBut.addEventListener ("click", calculTotals)
 
-function validCode (coupon) {
+let cupon = document.getElementById ('textPromo').value;
+
+let valiCoup = validcode (cupon);
+
+if (valiCoup == "NOTAX") {
+    total = subtotal; }
+
+if (valiCoup == "FIFTYFIFTY") {
+    subtotal = (pantsExtPrice + shirtsExtPrice)/2;
+    tax = subtotal * 13/100;
+    total = subtotal + tax; }
+
+if (valiCoup !== "NOTAX" || "FIFTYFIFTY") {
+    document.getElementById ('textPromo').innerHTML = "Please, Enter a Valid Code";
+}
+    
+function validcode (cupon) {
     let firstCode = "NOTAX";
     let secondCode = "FIFTYFIFTY";
     let validIn = document.getElementById ('promoText').value; 
@@ -111,17 +116,11 @@ function validCode (coupon) {
     if (validIn.toUpperCase() == secondCode.toUpperCase()){
         return secondCode;}
     else {
-        document.getElementById ('promoText').innerHTML = "Please, Enter a Valid Coupon";
+        document.getElementById ('promoText').innerHTML = "Please, Enter a Valid Code";
         return false;}
 }
-function showCode () {
-    document.getElementById ('promoText').display = "inline-block";
-}
 
-calcBut.addEventListener ("click", calculTotals)
-promoButton.addEventListener ("click", function(){
-    promoText.classList.toggle ('show')
-});
+promoButton.addEventListener ("click", validcode);
 
 /* function minus () {
     let label = document.getElementById ("pantsQty");
